be1ff148af96d3990efc511349f19e0c845598b7
ronaldsalili1 <ronald@beautitag.com>
2024-05-14 03:09:54 +0800

    Added grade submission processor

--- workers/spawn_notification/processors/index.js
+++ workers/spawn_notification/processors/index.js
@@ -1,7 +1,9 @@
 import account_creation from './account_creation.js';
 import password_reset_request from './password_reset_request.js';
+import grade_submission from './grade_submission.js';
 
 export default {
     account_creation,
     password_reset_request,
+    grade_submission,
 };
